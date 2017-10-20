ddf4ad8d54a06906ea83d14e4feb34fa7eb08cdb
Jason Goldberger <jlgoldb2@asu.edu>
2017-10-20 00:32:25 -0700

    removed data_files from setup.py

--- setup.py
+++ setup.py
@@ -49,9 +49,6 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-     data_files=[
-         ('my_data', ['data/data_file'])
-        ],
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     #   py_modules=["my_module"],
